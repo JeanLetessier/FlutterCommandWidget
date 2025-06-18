0ea2c4dd6477558fc8fb723adcd8e2fc35be08ec
Jean <jean.letessier@protonmail.com>
2025-06-18 11:46:14 +0200

    Finished laying out elements. Need to finish styling text now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  var commandItems = <CommandItem>[
+    CommandItem(amount: 1, price: 100.0, name: 'Adulte'),
+    CommandItem(amount: 1, price: 100.0, name: 'Adulte'),
+    CommandItem(amount: 1, price: 75.0, name: 'Enfant 4-11 ans'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Container(
           color: Colors.white,
-          margin: EdgeInsets.all(10),
-          child: Row(
+          margin: EdgeInsets.all(15),
+          child: Column(
             children: [
-              SizedBox(
-                width: 5,
-                height: 43,
-                child: ColoredBox(color: Colors.black),
+              // Top command information row
+              Row(
+                children: [
+                  SizedBox(
+                    width: 3,
+                    height: 35,
+                    child: ColoredBox(color: Colors.black),
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 9)),
+                  Column(
+                    children: [
+                      Text("Commande N.254", textAlign: TextAlign.left),
+                      Text('Anonyme', textAlign: TextAlign.left),
+                    ],
+                  ),
+                  Spacer(),
+                  RoundedSquareIconButton(
+                    onPressed: () {
+                      print('person button pressed');
+                    },
+                    icon: Icon(Icons.person_outline_rounded),
+                    backgroundColor: Color.fromRGBO(127, 29, 29, 1),
+                    iconColor: Color.fromRGBO(247, 241, 241, 1),
+                  ),
+                ],
               ),
-              Padding(padding: EdgeInsets.only(right: 9)),
-              // Column(children: [Text("Commande N.254")]),
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              // item list
+              Column(
+                children: [for (var commandItem in commandItems) commandItem],
+              ),
+              Spacer(),
+              // Button row
+              Row(
+                children: [
+                  // Delete button
+                  RoundedSquareIconButton(
+                    icon: Icon(Icons.delete_outline_rounded),
+                    backgroundColor: Color.fromRGBO(185, 28, 28, 1),
+                    iconColor: Color.fromRGBO(247, 241, 241, 1),
+                    onPressed: () {
+                      print('delete button pressed');
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 7)),
+                  // Card button
+                  RoundedSquareIconButton.outlined(
+                    icon: Icon(Icons.credit_card_outlined),
+                    backgroundColor: Color.fromRGBO(249, 250, 251, 1),
+                    iconColor: Color.fromRGBO(55, 65, 81, 1),
+                    onPressed: () {
+                      print('card button pressed');
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 7)),
+                  Expanded(
+                    child: RoundedSquareElevatedButton(
+                      backgroundColor: Color.fromRGBO(4, 120, 87, 1),
+                      text: 'Payer 275,00€',
+                      textColor: Color.fromRGBO(247, 241, 241, 1),
+                      onPressed: () {
+                        print('pressed pay button');
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -31,85 +99,120 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class DeleteButton extends StatelessWidget {
-  const DeleteButton({super.key});
+class RoundedSquareElevatedButton extends StatelessWidget {
+  final String text;
+  final Color backgroundColor;
+  final Color textColor;
+  final VoidCallback onPressed;
+
+  const RoundedSquareElevatedButton({
+    super.key,
+    required this.text,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      decoration: ShapeDecoration(
-        color: Color.fromRGBO(185, 28, 28, 1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(5),
-        ),
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        backgroundColor: backgroundColor,
+        foregroundColor: textColor,
       ),
-      child: IconButton(
-        onPressed: () {
-          print('delete pressed');
-        },
-        icon: Icon(Icons.delete_outline_rounded),
-        color: Colors.white,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 26, vertical: 12),
+        child: Text(text),
       ),
     );
   }
 }
 
-class CardButton extends StatelessWidget {
-  const CardButton({super.key});
+class RoundedSquareIconButton extends StatelessWidget {
+  final Icon icon;
+  final Color backgroundColor;
+  final Color iconColor;
+  final VoidCallback onPressed;
+  final bool outlined;
+
+  const RoundedSquareIconButton({
+    super.key,
+    required this.icon,
+    required this.backgroundColor,
+    required this.iconColor,
+    required this.onPressed,
+    this.outlined = false,
+  });
+
+  const RoundedSquareIconButton.outlined({
+    super.key,
+    required this.icon,
+    required this.backgroundColor,
+    required this.iconColor,
+    required this.onPressed,
+    this.outlined = true,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(5),
+    if (!outlined) {
+      return IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: iconColor,
+        style: IconButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: backgroundColor,
         ),
-      ),
-      child: Container(
-        child: IconButton(
-          onPressed: () {
-            print('card pressed');
-          },
-          icon: Icon(Icons.credit_card_outlined),
-          color: Colors.black,
+      );
+    } else {
+      return IconButton.outlined(
+        onPressed: onPressed,
+        icon: icon,
+        color: iconColor,
+        style: IconButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          backgroundColor: backgroundColor,
         ),
-      ),
-    );
+      );
+    }
   }
 }
 
-class PayButton extends StatelessWidget {
-  const PayButton({super.key});
+class CommandItem extends StatelessWidget {
+  final int amount;
+  final double price;
+  final String name;
+
+  const CommandItem({
+    super.key,
+    required this.amount,
+    required this.price,
+    required this.name,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      decoration: ShapeDecoration(
-        color: Color.fromRGBO(4, 120, 87, 1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(5),
-        ),
-      ),
-      child: Container(
-        margin: EdgeInsetsDirectional.symmetric(
-          horizontal: 25.0,
-          vertical: 3.0,
-        ),
-        child: TextButton(
-          onPressed: () {
-            print('pay pressed');
-          },
-          child: Text(
-            'Payer 275.00€',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-            ),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+          child: Row(
+            children: [
+              Text('${amount}x'),
+              Padding(padding: EdgeInsets.only(left: 20)),
+              Text(name),
+              Spacer(),
+              Text(
+                '${price.toStringAsFixed(2).replaceAll(RegExp(r'\.'), ',')}€',
+              ),
+            ],
           ),
         ),
-      ),
+        Divider(color: Color.fromRGBO(209, 213, 219, 1)),
+      ],
     );
   }
 }
